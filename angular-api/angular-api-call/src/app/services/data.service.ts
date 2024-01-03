7d2c46155c03dff51b730b9dcf55db209a762404
GauriSP <gpisharady@gmail.com>
2024-01-03 23:22:19 +0530

    working on api calls in angular

---
+++ angular-api/angular-api-call/src/app/services/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class DataService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/users`);
+  }
+
+  // Add more methods for other API endpoints as needed
+}
